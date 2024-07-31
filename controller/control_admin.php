fba83d42a16e546ce904622c2c819f9900b70863
PH37757 <156052088+PH37757@users.noreply.github.com>
2024-07-31 16:58:29 +0700

    Add files via upload

---
+++ controller/control_admin.php
@@ -0,0 +1,41 @@
+<?php
+
+function themdm(){
+    
+}
+function hiendm(){
+    
+}
+function suadm(){
+    
+}
+function xoadm(){
+
+}
+function themsp(){
+    
+}
+function hiensp(){
+    
+}
+function suasp(){
+    
+}
+function xoasp(){
+    
+}
+function themtk(){
+    
+}
+function hientk(){
+    
+}
+function suatk(){
+    
+}
+function xoatk(){
+
+}
+
+
+?>
